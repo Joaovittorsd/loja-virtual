ce9194a5d7a587b3515c0965a7ee2807c69c5898
Joaovittorsd <vittor.sdbr@gmail.com>
2024-12-16 16:34:32 -0300

    first commit

---
+++ src/main/java/com/projeto/sistema/controle/EstadoControle.java
@@ -0,0 +1,48 @@
+package com.projeto.sistema.controle;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.projeto.sistema.modelos.Estado;
+import com.projeto.sistema.repositorios.EstadoRepositorio;
+
+@Controller
+public class EstadoControle {
+	
+	@Autowired
+	private EstadoRepositorio estadoRepositorio;
+	
+	@GetMapping("/cadastro-estado")
+	public ModelAndView cadastrar(Estado estado) {
+		ModelAndView mv = new ModelAndView("administrativo/estados/cadastro");
+		return mv.addObject("estado", estado);
+	}
+	
+	@GetMapping("/listar-estado")
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView("administrativo/estados/lista");
+		return mv.addObject("listarEstados", estadoRepositorio.findAll());
+	}
+	
+	@PostMapping("/salvar-estado")
+	public ModelAndView salvar(Estado estado, BindingResult result) {
+		if (result.hasErrors()) {
+			return cadastrar(estado);
+		}
+		estadoRepositorio.saveAndFlush(estado);
+		return cadastrar(new Estado());
+	}
+	
+	@GetMapping("/editar-estado/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id) {
+		Optional<Estado> estado = estadoRepositorio.findById(id);
+		return cadastrar(estado.get());
+	}
+}
